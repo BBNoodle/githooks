274f510a503ac79a287ff482d9c8e49bce91dea1
兔子先生😉 <oncwnuDUXQn4lX_NOOE1j8nFqJVs@git.weixin.qq.com>
2021-04-14 09:01:35 +0800

    1

--- setup.py
+++ setup.py
@@ -10,18 +10,33 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="xt-githooks",
-    version="0.2.2",
-    author="Maoxinteng",
+    version="0.2.2.1",
+    author="BBNoodle",
     author_email="1214403402@qq.com",
-    description="Git Hooks Pre-receive",
+    description="Git pre-receive hook to check commits",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/BBNoodle/githooks.git",
+    keywords=(
+        'git git-hook python pre-receive hook'
+    ),
+    entry_points={
+        'console_scripts': [
+            'pre-receive=githooks:main',
+        ],
+    },
     packages=setuptools.find_packages(),
     classifiers=[
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
+        "Operating System :: OS Independent",
+        'Topic :: Software Development',
+        'Topic :: Utilities',
     ],
-    python_requires=">=3.6"
+    python_requires=">=3.6",
+    include_package_data=True
 )
